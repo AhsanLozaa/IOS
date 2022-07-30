b8f0c1016abc88be7cae7e77106ac664217e35c7
AhsanLozaa <ahsanmohamed97@gmail.com>
2022-07-30 13:04:25 +0530

    initial commit - creating a scrum meeting app

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ahsan Ilyas on 2022-07-30.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
